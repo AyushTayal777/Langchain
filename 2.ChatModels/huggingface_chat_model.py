42acd314d7524bd88a667bc51957429e7b9473d3
Ayush Tayal <ayushtayal685@gmail.com>
2026-08-12 22:07:21 +0530

    huggingface

---
+++ 2.ChatModels/huggingface_chat_model.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+load_dotenv()
+
+llm=HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model=ChatHuggingFace(llm=llm)
+result=model.invoke("Tell me about maths")
+print(result.content)
