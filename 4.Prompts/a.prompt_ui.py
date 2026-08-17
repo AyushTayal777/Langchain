5564e3e68ded5b451426f7d332592e2c168928db
Ayush Tayal <ayushtayal685@gmail.com>
2026-08-17 16:17:29 +0530

    new method

--- 4.Prompts/a.prompt_ui.py
+++ 4.Prompts/a.prompt_ui.py
@@ -1,15 +1,28 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
 from dotenv import load_dotenv
-
+from langchain_core.prompts import PromptTemplate,load_prompt
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
 
-st.header("Research Tool")
+st.header('Reasearch Tool')
 
-user_input = st.text_input("Enter your prompt")
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
-if st.button("Summarize"):
-    result = model.invoke(user_input)
-    st.write(result.content)
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = load_prompt('template.json')
+
+
+
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result.text)
